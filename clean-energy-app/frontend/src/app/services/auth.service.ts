22d3d5ec314ca75ead4098aec2509a3ababd7ea8
ushakundar <ukundar@charlotte.edu>
2025-05-02 09:36:25 -0400

    All environment changes

--- clean-energy-app/frontend/src/app/services/auth.service.ts
+++ clean-energy-app/frontend/src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/auth/login';
+  private apiUrl = `${environment.baseUrl}/api/auth/login`;
 
   constructor(private http: HttpClient, private router: Router) {}
 
